288b842e5beab3c5a4c8a60f52ef3707482cce53
Marcelo Pinheiro Meireles <marcelopimeireles@gmail.com>
2020-05-20 15:16:27 +0100

    User starred list

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# reactnative101
+# Github Stars
+## A React Native App that searches Github users and list starred projects
+
+#### Main features
+- React Native styled components
+- Mobile 'localstorage'
+- integration with Github API
+- Stack Navigation
+
+![Main view](/screenshot/users.jpg?raw=true "Main view")
+![User view](/screenshot/user.jpg?raw=true "User view")
--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -4,6 +4,20 @@ import { View } from 'react-native';
 import api from '../../services/api';
 import '../../config/ReactotronConfig';
 
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
+
 const User = ({ navigation, route }) => {
   const { user } = route.params;
   navigation.setOptions({ title: user.name });
@@ -19,7 +33,29 @@ const User = ({ navigation, route }) => {
     loadStars();
   }, [setStars]);
 
-  return <View />;
+  return (
+    <Container>
+      <Header>
+        <Avatar source={{ uri: user.avatar }} />
+        <Name>{user.name}</Name>
+        <Bio>{user.bio}</Bio>
+      </Header>
+
+      <Stars
+        data={stars}
+        keyExtractor={(star) => String(star.id)}
+        renderItem={({ item }) => (
+          <Starred>
+            <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+            <Info>
+              <Title>{item.name}</Title>
+              <Author>{item.owner.login}</Author>
+            </Info>
+          </Starred>
+        )}
+      />
+    </Container>
+  );
 };
 
 User.propTypes = {
