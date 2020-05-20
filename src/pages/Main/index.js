ee0f79e95961b96e22a41abdc9dc555b630da348
Marcelo Pinheiro Meireles <marcelopimeireles@gmail.com>
2020-05-20 10:48:54 +0100

    main styled componets

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Form, Input, SubmitButton } from './styles';
 
 import '../../config/ReactotronConfig';
 
 const Main = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitaliza="none"
+          placeholder="Adicionar usuário"
+        />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#FFF" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 };
 
 export default Main;
