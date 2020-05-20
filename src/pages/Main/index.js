36e7991dbed66814aae65ea464dfc9dab46313e5
Marcelo Pinheiro Meireles <marcelopimeireles@gmail.com>
2020-05-20 11:09:46 +0100

    github api service

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,21 +1,46 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '../../services/api';
 
 import { Container, Form, Input, SubmitButton } from './styles';
 
 import '../../config/ReactotronConfig';
 
 const Main = () => {
+  const [users, setUsers] = useState([]);
+  const [newUser, setNewUser] = useState('');
+
+  const handleAddUser = async () => {
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    setUsers([...users, data]);
+    setNewUser('');
+
+    Keyboard.dismiss();
+  };
+
   return (
     <Container>
       <Form>
         <Input
           autoCorrect={false}
-          autoCapitaliza="none"
+          autoCapitalize="none"
           placeholder="Adicionar usuário"
+          value={newUser}
+          onChangeText={(text) => setNewUser(text)}
+          returKeyType="send"
+          onSubmitEditing={handleAddUser}
         />
-        <SubmitButton>
+        <SubmitButton onPress={handleAddUser}>
           <Icon name="add" size={20} color="#FFF" />
         </SubmitButton>
       </Form>
